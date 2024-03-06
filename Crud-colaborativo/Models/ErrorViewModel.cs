d51dbab2eff72a2a1ee81119d48369dab40cc8e6
Juan Pinilla <juan.pinilla@crowe.com.co>
2024-03-06 14:57:56 -0500

    Agregar archivos de proyecto.

---
+++ Crud-colaborativo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Crud_colaborativo.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
